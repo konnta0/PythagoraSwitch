3bdc87c8a0451ac8323fd068143d94a1132159f4
konnta <konnta0@gmail.com>
2021-08-31 00:50:47 +0900

    wip importer

--- src/PythagoraSwitch/Player/PsImporter.cs
+++ src/PythagoraSwitch/Player/PsImporter.cs
@@ -1,22 +1,38 @@
 using System.Collections.Generic;
+using System.IO;
 using konnta0.Exceptions;
+using Microsoft.Extensions.Logging;
 using PythagoraSwitch.Player.Interfaces;
 using PythagoraSwitch.WebRequest.Recorder.Interfaces;
+using YamlDotNet.Serialization;
 
 namespace PythagoraSwitch.Player
 {
     public class PsImporter : IPsImporter
     {
         private readonly IPsImporterConfig _config;
+        private readonly ILogger<PsImporter> _logger;
 
-        public PsImporter(IPsImporterConfig config)
+        public PsImporter(IPsImporterConfig config, ILogger<PsImporter> logger)
         {
             _config = config;
+            _logger = logger;
         }
         
         public (IList<IPsRecordContent>, IErrors) Handle(string path)
         {
-            throw new System.NotImplementedException();
+            if (!File.Exists(path))
+            {
+                return (default, Errors.New<FileNotFoundException>());
+            }
+
+            IList<IPsRecordContent> records = default;
+            var error = Errors.Try(delegate
+            {
+                var deserializer = new Deserializer();
+                records = deserializer.Deserialize<IList<IPsRecordContent>>(path);
+            });
+            return (records, error);
         }
     }
 }
