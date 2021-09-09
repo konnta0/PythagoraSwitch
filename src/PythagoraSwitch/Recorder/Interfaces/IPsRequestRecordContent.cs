c58759aeb4b942e341686cda6a99858c91706a4a
konnta <konnta0@gmail.com>
2021-09-10 00:31:31 +0900

    feat: with headers

--- src/PythagoraSwitch/Recorder/Interfaces/IPsRequestRecordContent.cs
+++ src/PythagoraSwitch/Recorder/Interfaces/IPsRequestRecordContent.cs
@@ -1,13 +1,18 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace PythagoraSwitch.Recorder.Interfaces
 {
     public interface IPsRequestRecordContent
     {
         TimeSpan Interval { get; set; }
+
         string EndPoint { get; set; }
+
         string Method { get; set; }
-        
+
+        IEnumerable<KeyValuePair<string, IEnumerable<string>>> Headers { get; set; }
+
         PythagoraSwitch.WebRequest.Interfaces.IPsWebRequestContent RequestContent { get; set; }
     }
 }
