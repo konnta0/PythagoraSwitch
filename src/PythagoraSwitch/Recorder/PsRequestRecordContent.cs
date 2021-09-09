4e78951a507f6d58599c7c284d87e7d66918eb45
konnta <konnta0@gmail.com>
2021-09-10 00:17:43 +0900

    fix: nits

--- src/PythagoraSwitch/Recorder/PsRequestRecordContent.cs
+++ src/PythagoraSwitch/Recorder/PsRequestRecordContent.cs
@@ -19,12 +19,5 @@ namespace PythagoraSwitch.Recorder
         }
 
         public DateTime GetRequestStartAt() => _requestStartAt;
-        
-        
-        public static PsRequestRecordContent Create(TimeSpan interval, string endPoint, string method, IPsWebRequestContent requestContent)
-        {
-            return new PsRequestRecordContent
-                {Interval = interval, EndPoint = endPoint, Method = method, RequestContent = requestContent};
-        }
     }
 }
