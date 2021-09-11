254982d2bfcf1ee4345b834ac476d1d4f57ec65b
konnta <konnta0@gmail.com>
2021-09-12 01:03:11 +0900

    wip

---
+++ tests/PythagoraSwitch.Test/Importer/PsPlayerTests.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace PythagoraSwitch.Test.Importer
+{
+    public class PsPlayerTests
+    {
+        [Fact]
+        public void Handle()
+        {
+            
+        }
+    }
+}
