6222b703bf8e985d5a2906d2b4bcb629720543aa
konnta <konnta0@gmail.com>
2021-10-03 01:02:27 +0900

    feat: create empty player project, test

---
+++ src/PythagoraSwitch.Player/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PythagoraSwitch.Player
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
