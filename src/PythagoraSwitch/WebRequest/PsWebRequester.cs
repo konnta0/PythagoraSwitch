6fbb9b490fbfaed98ef0ce5dceea634244b13eb8
konnta <konnta0@gmail.com>
2021-10-11 00:56:35 +0900

    refactor: split file

--- src/PythagoraSwitch/WebRequest/PsWebRequester.cs
+++ src/PythagoraSwitch/WebRequest/PsWebRequester.cs
@@ -290,45 +290,4 @@ namespace PythagoraSwitch.WebRequest
 
         public Action<bool> OnChangeRequesting { get; set; }
     }
-    
-    internal static class UrlHelpers
-    {
-        internal static string ToQueryString(this IPsWebGetRequestContent request, string separator = ",")
-        {
-            if (request == null)
-                throw new ArgumentNullException("request");
-
-            // Get all properties on the object
-            var properties = request.GetType().GetProperties()
-                .Where(x => x.CanRead)
-                .Where(x => x.GetValue(request, null) != null)
-                .ToDictionary(x => x.Name, x => x.GetValue(request, null));
-
-            // Get names for all IEnumerable properties (excl. string)
-            var propertyNames = properties
-                .Where(x => !(x.Value is string) && x.Value is IEnumerable)
-                .Select(x => x.Key)
-                .ToList();
-
-            // Concat all IEnumerable properties into a comma separated string
-            foreach (var key in propertyNames)
-            {
-                var valueType = properties[key].GetType();
-                var valueElemType = valueType.IsGenericType
-                    ? valueType.GetGenericArguments()[0]
-                    : valueType.GetElementType();
-                if (valueElemType != null && (valueElemType.IsPrimitive || valueElemType == typeof(string)))
-                {
-                    var enumerable = properties[key] as IEnumerable;
-                    properties[key] = string.Join(separator, enumerable.Cast<object>());
-                }
-            }
-
-            // Concat all key/value pairs into a string separated by ampersand
-            return string.Join("&", properties
-                .Select(x => string.Concat(
-                    Uri.EscapeDataString(x.Key), "=",
-                    Uri.EscapeDataString(x.Value.ToString()))));
-        }
-    }    
 }
