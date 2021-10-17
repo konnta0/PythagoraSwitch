e15d5b219a46d445bf48f9cdf918e53154a1d6f6
konnta <konnta0@gmail.com>
2021-10-18 00:49:25 +0900

    refactor: reformat

--- tests/PythagoraSwitch.Test/WebRequest/WebRequestHandlerTests.cs
+++ tests/PythagoraSwitch.Test/WebRequest/WebRequestHandlerTests.cs
@@ -13,32 +13,35 @@ namespace PythagoraSwitch.Test.WebRequest
         public async void SimpleGetRequestTest()
         {
             var psHttpClientFactoryMock = TestHelper.CreatePsHttpClientFactoryMock(HttpMethod.Get, "/api/dummy/get");
-            
+
             var loggerMock = TestHelper.CreateLoggerMock();
             var networkAccess = TestHelper.CreateNetworkAccessMock();
             var config = TestHelper.CreateConfigMock();
             using var tokenSource = new CancellationTokenSource();
             var requestQueue = TestHelper.CreateRequestQueueMock(tokenSource.Token);
             var interceptor = TestHelper.CreateWebRequestInterceptor();
-            var requester = new WebRequestHandler(
+            var handler = new WebRequestHandler(
                 loggerMock.Object,
                 networkAccess.Object,
                 config.Object,
                 requestQueue.Object,
                 interceptor.Object
-                );
-            var (response, error) = await requester.GetAsync<DummyGetRequestContent, DummyGetResponseContent>(new Uri("http://pstest/api/dummy/get"), new DummyGetRequestContent());
+            );
+            var (response, error) =
+                await handler.GetAsync<DummyGetRequestContent, DummyGetResponseContent>(
+                    new Uri("http://pstest/api/dummy/get"), new DummyGetRequestContent());
             tokenSource.Cancel();
-            Assert.False(Errors.IsOccurred(error), $"message: {error?.Exception.Message} \n trace: {error?.Exception.StackTrace}");
+            Assert.False(Errors.IsOccurred(error),
+                $"message: {error?.Exception.Message} \n trace: {error?.Exception.StackTrace}");
             Assert.NotNull(response);
             Assert.Equal("hogehoge", response.hoge);
         }
-        
+
         [Fact(Timeout = 300)]
         public async void SimplePostRequestTest()
         {
             var psHttpClientFactoryMock = TestHelper.CreatePsHttpClientFactoryMock(HttpMethod.Post, "/api/dummy/post");
-            
+
             var loggerMock = TestHelper.CreateLoggerMock();
             var networkAccess = TestHelper.CreateNetworkAccessMock();
             var config = TestHelper.CreateConfigMock();
@@ -46,16 +49,19 @@ namespace PythagoraSwitch.Test.WebRequest
             var requestQueue = TestHelper.CreateRequestQueueMock(tokenSource.Token);
             var interceptor = TestHelper.CreateWebRequestInterceptor();
 
-            var requester = new WebRequestHandler(
+            var handler = new WebRequestHandler(
                 loggerMock.Object,
                 networkAccess.Object,
                 config.Object,
                 requestQueue.Object,
                 interceptor.Object
-                );
-            var (response, error) = await requester.PostAsync<DummyPostRequestContent, DummyPostResponseContent>(new Uri("http://pstest/api/dummy/post"), new DummyPostRequestContent());
+            );
+            var (response, error) =
+                await handler.PostAsync<DummyPostRequestContent, DummyPostResponseContent>(
+                    new Uri("http://pstest/api/dummy/post"), new DummyPostRequestContent());
             tokenSource.Cancel();
-            Assert.False(Errors.IsOccurred(error), $"message: {error?.Exception.Message} \n trace: {error?.Exception.StackTrace}");
+            Assert.False(Errors.IsOccurred(error),
+                $"message: {error?.Exception.Message} \n trace: {error?.Exception.StackTrace}");
             Assert.NotNull(response);
             Assert.Equal("hogehoge", response.hoge);
         }
