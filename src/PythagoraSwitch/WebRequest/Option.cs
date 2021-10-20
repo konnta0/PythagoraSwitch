2dd58512237d65efcbfb4c2decbf05e7730e05d5
konnta <konnta0@gmail.com>
2021-10-21 00:15:26 +0900

    refactor: remove

--- src/PythagoraSwitch/WebRequest/Option.cs
+++
@@ -1,9 +0,0 @@
-using PythagoraSwitch.WebRequest.Interfaces;
-
-namespace PythagoraSwitch.WebRequest
-{
-    public class Option : IOption
-    {
-        public RequestInterceptors RequestInterceptors { get; } = new RequestInterceptors();
-    }
-}
