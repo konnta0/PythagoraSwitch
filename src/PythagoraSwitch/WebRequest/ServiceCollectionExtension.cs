ba7f2aa3337a9f8f6091d279be0a1b6b1d0c7744
konnta <konnta0@gmail.com>
2021-10-27 00:02:42 +0900

    service  collection extension

--- src/PythagoraSwitch/WebRequest/ServiceCollectionExtension.cs
+++ src/PythagoraSwitch/WebRequest/ServiceCollectionExtension.cs
@@ -1,4 +1,3 @@
-using System;
 using Microsoft.Extensions.DependencyInjection;
 using PythagoraSwitch.Recorder;
 using PythagoraSwitch.Recorder.Interfaces;
@@ -11,18 +10,34 @@ namespace PythagoraSwitch.WebRequest
     {
         public static IServiceCollection AddPythagoraSwitch(this IServiceCollection serviceCollection)
         {
-            serviceCollection.AddSingleton<IHttpClientFactory, HttpClientFactory>();
-            serviceCollection.AddSingleton<ISerializer, JsonSerializer>();
-            serviceCollection.AddSingleton<IRequestQueue, RequestQueue>();
-            serviceCollection.AddSingleton<IWebRequestHandler, WebRequestHandler>();
-            serviceCollection.AddSingleton<IRecorder, Recorder.Recorder>();
-            serviceCollection.AddSingleton<IWebRequestExporter, WebRequestExporter>();
-            return serviceCollection;
+            return serviceCollection.AddPythagoraSwitch(new DefaultConfig(), new EmptyNetworkAccess(),
+                new EmptyRequestInterceptor());
         }
 
-        public static IServiceCollection AddPythagoraSwitchConfig(this IServiceCollection serviceCollection, IConfig config)
+        public static IServiceCollection AddPythagoraSwitch(this IServiceCollection serviceCollection, IConfig config)
         {
-            return serviceCollection.AddSingleton(provider => config);
+            return serviceCollection.AddPythagoraSwitch(config, new EmptyNetworkAccess(), new EmptyRequestInterceptor());
+        }
+
+        public static IServiceCollection AddPythagoraSwitch(this IServiceCollection serviceCollection, IConfig config, IWebRequestRecorderInterceptor recorderInterceptor)
+        {
+            return serviceCollection.AddPythagoraSwitch(config, new EmptyNetworkAccess(), recorderInterceptor);
+        }
+
+        public static IServiceCollection AddPythagoraSwitch(this IServiceCollection serviceCollection, IConfig config, INetworkAccess network, IWebRequestRecorderInterceptor recorderInterceptor)
+        {
+            serviceCollection.AddScoped(x => network ?? new EmptyNetworkAccess());
+            serviceCollection.AddSingleton<IRequestQueue, RequestQueue>();
+            serviceCollection.AddSingleton<IWebRequestHandler, WebRequestHandler>();
+            serviceCollection.AddSingleton(x => config ?? new DefaultConfig());
+            serviceCollection.AddScoped<IHttpClientFactory, HttpClientFactory>();
+            serviceCollection.AddSingleton<ISerializer, JsonSerializer>();
+            serviceCollection.AddSingleton<IWebRequestInterceptor, WebRequestInterceptor>();
+
+            serviceCollection.AddSingleton<IRecorder, Recorder.Recorder>();
+            serviceCollection.AddSingleton<IWebRequestExporter, WebRequestExporter>();
+            serviceCollection.AddSingleton(x => recorderInterceptor ?? new EmptyRequestInterceptor());
+            return serviceCollection;
         }
     }
 }
