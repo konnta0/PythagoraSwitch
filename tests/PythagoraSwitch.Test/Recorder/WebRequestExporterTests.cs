a24f84034109c50c0ecfefedd34ec946d62b4c1d
konnta0 <konnta0@gmail.com>
2021-11-19 01:17:55 +0900

    WIP: ExporterTest (GenerateScenario)

--- tests/PythagoraSwitch.Test/Recorder/WebRequestExporterTests.cs
+++ tests/PythagoraSwitch.Test/Recorder/WebRequestExporterTests.cs
@@ -1,18 +1,39 @@
+using System.IO;
 using konnta0.Exceptions;
 using PythagoraSwitch.Recorder;
 using Xunit;
+using Xunit.Abstractions;
 
 namespace PythagoraSwitch.Test.Recorder
 {
     public class WebRequestExporterTests
     {
+        private readonly ITestOutputHelper _testOutputHelper;
+
+        public WebRequestExporterTests(ITestOutputHelper testOutputHelper)
+        {
+            _testOutputHelper = testOutputHelper;
+        }
+
         [Fact]
         internal void HandleTest()
         {
-            var exporter = new WebRequestExporter(new DefaultExporterConfig(), LoggerFactory.Create<WebRequestExporter>());
+            var exporter = new WebRequestExporter(new DefaultExporterConfig(), new ScenarioTemplate(), LoggerFactory.Create<WebRequestExporter>());
             var recordContents = DummyRecordFactory.CreateByInterface();
             var (path, errors) = exporter.Handle(recordContents);
             Assert.False(Errors.IsOccurred(errors));
+        }
+
+        [Fact]
+        internal void HandleGenerateScenarioTest()
+        {
+            var exporter = new WebRequestExporter(new DefaultExporterConfig(), new ScenarioTemplate(), LoggerFactory.Create<WebRequestExporter>());
+            var recordContents = DummyRecordFactory.CreateByInterface();
+            var scenarioName = "DummyScenario";
+            var (path, errors) = exporter.Handle(scenarioName, recordContents);
+            Assert.False(Errors.IsOccurred(errors));
+            Assert.Contains($"{scenarioName}.Generated.cs", path);
+            _testOutputHelper.WriteLine(File.ReadAllText(path));
         }
     }
 }
