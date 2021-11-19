f6285b720b4da4ffddb988d78218c098719f4e7a
konnta0 <konnta0@gmail.com>
2021-11-20 00:01:11 +0900

    WIP: template

--- src/PythagoraSwitch/Recorder/ScenarioTemplate.cs
+++ src/PythagoraSwitch/Recorder/ScenarioTemplate.cs
@@ -4,16 +4,19 @@ namespace PythagoraSwitch.Recorder
 {
     internal sealed class ScenarioTemplate : IScenarioTemplate
     {
-        public string GetSyntax => 
-            @"<ul id='products'>
-  {{ for product in products }}
-    <li>
-      <h2>{{ product.name }}</h2>
-           Price: {{ product.price }}
-           {{ product.description | string.truncate 15 }}
-    </li>
-  {{ end }}
-</ul>
+        public string GetSyntax => @"
+// This file generated. Do not edit
+namespace PythagoraSwitch.Recorder.Scenario
+{
+    IList<
+    public class {{scenario_name}} : IScenario {
+    }
+
+{{for content in contents}}
+    public class {{request_content_type_to_string content.request_content_type}} : {{content_implement_interface_string content.method}} {
+}
+{{end}}
+}
 ";
     }
 }
